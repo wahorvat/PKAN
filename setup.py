8badd3857d72221ec8c9619ba194f2f36ea0d26b
wahorvat <wahorvat@gmail.com>
2024-06-09 17:43:56 -0400

    mol gen and configs

---
+++ setup.py
@@ -0,0 +1,34 @@
+"""Setup for pip package."""
+
+import unittest
+
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['lib...',
+                     'lib2...',                     
+]
+
+
+def ferminet_test_suite():
+  test_loader = unittest.TestLoader()
+  test_suite = test_loader.discover('exponet/tests', pattern='*_test.py')
+  return test_suite
+
+
+setup(
+    name='exponet',
+    version='0.1',
+    description=('A library to train networks to represent ground '
+                 'state wavefunctions of fermionic systems'),
+    url='https://github.com/wahorvat/exponet',
+    author='Will Horvat',
+    # Contained modules and scripts.
+    scripts=['bin/exponet'],
+    packages=find_packages(),
+    install_requires=REQUIRED_PACKAGES,
+    extras_require={'testing': ['flake8', 'pylint', 'pytest', 'pytype']},
+    platforms=['any'],
+    license='Apache 2.0',
+    test_suite='setup.ferminet_test_suite',
+)
